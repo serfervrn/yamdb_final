a05514091c1f7893e9347113e0c2e7f6b2a68018
Сергей Осенев <serfervrn@yandex.ru>
2022-08-13 14:29:44 +0300

    fix 33

--- api_yamdb/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-python3 /api_yamdb/manage.py migrate
-python3 /api_yamdb/manage.py loaddata fixtures.json
-python3 /api_yamdb/manage.py collectstatic --no-input
-gunicorn api_yamdb.wsgi:application --bind 0:8000
